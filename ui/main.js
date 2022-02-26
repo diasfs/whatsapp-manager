3216893b2e61f690edad60032926910eee9bd563
Fabio Dias <binho85@gmail.com>
2022-02-26 09:53:46 +0000

    install bootstrap

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router';
+import 'bootstrap';
+import './assets/scss/main.scss'
 
 const app = createApp(App);
 app.use(router);
