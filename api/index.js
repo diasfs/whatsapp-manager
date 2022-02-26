c2e9add5812c63c3e4a5406fdf3bfdb045aa5dcd
Fabio Dias <binho85@gmail.com>
2022-02-26 06:29:14 +0000

    create the server entrypoint

---
+++ api/index.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { connection } from './models/index.js'
+
+import UserRouter from './routes/user.js';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const __rootdir = dirname(__dirname);
+const __publicdir = path.join(__rootdir,'public');
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__publicdir));
+
+app.get('/api', (req, res) => {
+    res.json({
+        message: "Hello World"
+    });
+});
+
+app.use('/', UserRouter);
+
+
+const PORT = process.env.PORT||3000
+const HOST = process.env.HOST||'0.0.0.0'
+app.listen(PORT, HOST, () => {
+    console.log(`Server listen to ${HOST}:${PORT}`)
+});
