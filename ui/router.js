54506624f62c9d5ffef63f69ea85257baa6d06cf
Fabio Dias <binho85@gmail.com>
2022-02-26 22:56:35 +0000

    better authorizatin handling

--- ui/router.js
+++ ui/router.js
@@ -1,10 +1,24 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
 import routes from 'voie-pages';
+import SessionStorage from './lib/session-storage';
 
 const router = createRouter({
     history: createWebHashHistory(),
     routes
 });
 
+router.beforeEach(async (to, from) => {
+    const access_token = SessionStorage.getItem('access_token');
+    if (!access_token && !['login', 'forgoten', 'register'].includes(to.name)) {        
+        return {
+            name: 'login'
+        }
+    } else if (access_token && ['login', 'forgoten', 'register'].includes(to.name)) {
+        return {
+            name: 'index'
+        }
+    }
+    document.body.setAttribute('data-page', to.name);
+});
 
 export default router;
