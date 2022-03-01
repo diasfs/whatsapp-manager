616c1df705b33d12e23e4b6de444eb300ee81912
Fábio Dias <binho85@gmail.com>
2022-03-01 17:44:13 -0300

    whatsapp config page

---
+++ ui/components/configuracoes/whatsapp/connection.vue
@@ -0,0 +1,161 @@
+<template>
+    <div class="text-muted py-3 border-bottom">
+        <div class="d-flex mb-2">
+            <img
+                :src="connection.Contact.profilePictureUrl"
+                width="48"
+                height="48"
+                class="me-3 rounded-circle"
+                v-if="
+                    connection.Contact && connection.Contact.profilePictureUrl
+                "
+            />
+            <p class="pb-3 mb-0 small lh-sm">
+                <strong class="d-block text-gray-dark">{{
+                    connection.pushname
+                }}</strong>
+                <svg-icon
+                    type="mdi"
+                    :path="icons.whatsapp"
+                    size="1.25em"
+                ></svg-icon>
+                {{ phoneNumber }}
+            </p>
+        </div>
+        <div class="d-flex justify-content-md-start justify-content-center">
+            <button @click="reconectar" v-if="connection.state == 'DISCONNECTED'" class="btn bg-success text-white btn-sm mx-1">
+                <svg-icon
+                    type="mdi"
+                    :path="icons.connection"
+                    size="1.25em"
+                ></svg-icon>
+                Reconectar
+            </button>
+
+            <button
+                @click="desconectar"
+                class="btn bg-info text-white btn-sm mx-1"
+                v-if="connection.state=='CONNECTED'"
+            >
+                <svg-icon
+                    type="mdi"
+                    :path="icons.connection"
+                    size="1.25em"
+                ></svg-icon>
+                Desconectar
+            </button>
+            <button
+                @click="excluir"
+                class="btn bg-danger text-white btn-sm mx-1"
+            >
+                <svg-icon
+                    type="mdi"
+                    :path="icons.trash"
+                    size="1.25em"
+                ></svg-icon>
+                Excluir
+            </button>
+        </div>
+    </div>
+</template>
+<script>
+import SvgIcon from "@jamescoyle/vue-icon";
+import { mdiConnection, mdiTrashCanOutline, mdiWhatsapp } from "@mdi/js";
+import Swal from 'sweetalert2';
+import 'sweetalert2/src/sweetalert2.scss';
+import QRCode from 'qrcode';
+import SessionStorage from '../../../lib/session-storage';
+import api from '../../../lib/api';
+
+
+
+export default {
+    props: ["connection"],
+    components: {
+        SvgIcon,
+    },
+    data() {
+        return {
+            icons: {
+                connection: mdiConnection,
+                trash: mdiTrashCanOutline,
+                whatsapp: mdiWhatsapp,
+            },
+        };
+    },
+    computed: {
+        phoneNumber() {
+            return this.connection.wid.user.replace(
+                /(\d{2})(\d{2})(\d{4,5})(\d{4})/,
+                "$1 ($2) $3-$4"
+            );
+        },
+    },
+    methods: {
+        async reconectar() {
+            let swal = Swal.fire({                
+                text: "Carregando..."
+            });
+            let access_token = SessionStorage.getItem('access_token');
+            const events = new EventSource(`${location.origin}/api/whatsapp/conexao/${this.connection.id}/reconectar?access_token=${access_token}`)
+
+            events.addEventListener('message', async event => {
+                const data = JSON.parse(event.data);
+                let { qr } = data;
+                let url = await QRCode.toDataURL(qr);
+                
+                swal.update({
+                    imageUrl: url,
+                    text: null
+                });
+            });
+            events.addEventListener('authenticated', async event => {
+                const data = JSON.parse(event.data);                
+                swal.close();                
+                this.$emit('authenticated');
+            });
+
+            await swal;
+            events.close();
+        },
+        async excluir() {
+            let { isConfirmed } = await Swal.fire({
+                icon: "warning",
+                title: "Excluir Conexão?",
+                showCancelButton: true,
+                confirmButtonText: "Sim",
+                cancelButtonText: "Não",
+            });
+            alert(isConfirmed);
+        },
+        async desconectar() {
+            let { isConfirmed } = await Swal.fire({
+                icon: "warning",
+                title: "Desconectar?",
+                showCancelButton: true,
+                confirmButtonText: "Sim",
+                cancelButtonText: "Não",
+            });
+            if ( !isConfirmed) {
+                return;
+            }
+
+            try {
+                await api.post(`/whatsapp/conexao/${this.connection.id}/desconectar`)
+                this.$emit('disconnected');
+            } catch (err) {
+                let text = 'Não foi possível deconectar o número, por favor, tente novamente mais tarde.';
+                if (err.response && err.response.data) {
+                    text = err.response.data.error;
+                }
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text
+                });
+            }
+        },
+    },
+};
+</script>
+
---
+++ ui/pages/configuracoes/whatsapp/index.vue
@@ -0,0 +1,105 @@
+<template>
+    <main class="container">
+        <div
+            class="d-flex align-items-center p-3 my-3 text-white bg-secondary rounded shadow-sm"
+        >
+            <div class="lh-1">
+                <h1 class="h6 mb-0 text-white lh-1">Configurações</h1>
+                <small>WhatsApp</small>
+            </div>
+        </div>
+
+        <div class="my-3 p-3 bg-body rounded shadow-sm">
+            <h6 class="border-bottom pb-2 mb-0">Números conectados</h6>
+            
+            <div v-if="carregando">Carregando...</div>
+            <div v-else-if="connections.length == 0">
+                <p>Nenhuma conexão encontrada.</p>
+            </div>
+            <div v-if="connections.length > 0">
+                <Connection @authenticated="loadConnections" @disconnected="loadConnections" v-for="connection in connections" :key="connection.id" :connection="connection"></Connection>
+            </div>
+
+            <small class="d-flex justify-content-md-end justify-content-center  mt-3">
+                <button @click="novaConexao" class="btn btn-primary btn-sm text-white">
+                    <svg-icon type="mdi" :path="icons.whatsapp" size="1.5em"></svg-icon>
+                    Incluir novo número
+                </button>
+            </small>
+        </div>
+    </main>
+</template>
+<script>
+
+import api from '../../../lib/api';
+
+import Swal from 'sweetalert2';
+import 'sweetalert2/src/sweetalert2.scss';
+import QRCode from 'qrcode';
+import SessionStorage from '../../../lib/session-storage';
+
+import SvgIcon from "@jamescoyle/vue-icon";
+import { mdiWhatsapp } from "@mdi/js";
+import Connection from '../../../components/configuracoes/whatsapp/connection.vue'
+export default {
+    components: {
+        SvgIcon,
+        Connection
+    },
+    data() {
+        return {
+            icons: {
+                whatsapp: mdiWhatsapp
+            },
+            carregando: false,
+            connections: null
+        };
+    },
+    created() {
+        this.loadConnections();
+    },
+    methods: {
+        async loadConnections() {
+            this.carregando = true;
+            try {
+                let { data: connections } = await api.get('/whatsapp/conexoes');
+                this.connections = connections;
+            } catch (err) {
+                this.connections = [];
+                Swal.fire({
+                    title: 'Oops...',
+                    icon: 'error',
+                    text: "Não foi possível carregar as conexões"
+                });
+            }
+            this.carregando = false;
+        },
+        async novaConexao() {
+            let swal = Swal.fire({                
+                text: "Carregando..."
+            });
+            let access_token = SessionStorage.getItem('access_token');
+            const events = new EventSource(`${location.origin}/api/whatsapp/conexoes/nova?access_token=${access_token}`)
+
+            events.addEventListener('message', async event => {
+                const data = JSON.parse(event.data);
+                let { qr } = data;
+                let url = await QRCode.toDataURL(qr);
+                
+                swal.update({
+                    imageUrl: url,
+                    text: null
+                });
+            });
+            events.addEventListener('authenticated', async event => {
+                const data = JSON.parse(event.data);                
+                swal.close();                
+                this.loadConnections();
+            });
+
+            await swal;
+            events.close();
+        }
+    }
+};
+</script>
