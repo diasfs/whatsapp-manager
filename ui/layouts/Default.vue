cb9a04d5196989a329ae912dbe1f4358f53c0e51
Fábio Dias <binho85@gmail.com>
2022-03-01 17:45:02 -0300

    fix default layout columns

--- ui/layouts/Default.vue
+++ ui/layouts/Default.vue
@@ -1,15 +1,16 @@
 <template>
     <nav class="navbar navbar-white sticky-top bg-white flex-md-nowrap p-0">
-      <a class="bg-primary d-flex justify-content-center py-4 navbar-brand col-sm-12 col-md-3 col-lg-2 mr-0" href="#">
-          <img src="../assets/img/logo-white.svg" alt="Logo">
-          
-      </a>
-                    
+      <a class="bg-primary d-flex justify-content-center py-4 navbar-brand col-12 col-md-3 col-lg-2 me-0" href="#">
+          <img src="../assets/img/logo-white.svg" alt="Logo" height="24">          
+      </a>      
+      <div class="d-flex py-4 bg-primary col-12 col-md-9 col-lg-10 mx-0">
+          &nbsp;
+      </div>
     </nav>
     <div class="container-fluid">
         <div class="row">
             <Sidebar></Sidebar>
-            <main role="main" class="col-md-8 col-lg-9 ml-sm-auto col-lg-10 pt-3 px-4">
+            <main role="main" class="col-md-9 col-lg-9 ms-sm-auto col-lg-10 pt-3 px-4">
                 <slot></slot>
             </main>
         </div>
@@ -26,6 +27,7 @@
 <style lang="less" >
     #app {
         height: 100%;
+        background-color: var(--bs-gray-200);
     }
     
 </style>
